371eb88985985b24de358beb68d9094b68227072
raynardj <raynard@rasenn.com>
2019-06-24 00:09:45 +0800

    fix config bug

--- wed/app/__init__.py
+++ wed/app/__init__.py
@@ -2,6 +2,7 @@ import logging
 
 from flask import Flask
 from flask_appbuilder import AppBuilder, SQLA, IndexView,expose
+from wed import config
 
 """
  Logging configuration
@@ -11,7 +12,7 @@ logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
 logging.getLogger().setLevel(logging.DEBUG)
 
 app = Flask(__name__)
-app.config.from_object("config")
+app.config.from_object(config)
 db = SQLA(app)
 
 class CustomeIndexView(IndexView):
